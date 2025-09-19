6e86dc00bbb55d1d0cff2449a4167e20f4bd0362
Jasvinder SIngh <mrkaransingh3593@gmail.com>
2025-09-19 14:48:31 +0530

    added configs

---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HttpServer struct {
+	Host string
+	Port int  
+}
+
+type Config struct {
+	Env        string    `yaml:"env"`
+	StoragePath string    `yaml:"storage_path"`
+	HttpServer  HttpServer `yaml:"http_server"`
+}
+
+func NewConfig() (*Config, error) {
+	var configPath string
+
+	flag.StringVar(&configPath, "config", "", "path to config file")
+	flag.Parse()
+
+
+	if configPath == "" {
+		return nil, fmt.Errorf("config file path is required")
+	}
+
+	if _, err := os.Stat(configPath); err != nil {
+		return nil, fmt.Errorf("config file not found: %w", err)
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
